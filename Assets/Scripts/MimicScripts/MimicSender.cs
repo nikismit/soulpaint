c946aebb3d0ee391d6ed517d8d8564d711e9e876
NikitaKayal <nikita@improvive.com>
2021-09-24 11:09:00 +0200

    puppet backward compatibility and merge fixes

--- Assets/Scripts/MimicScripts/MimicSender.cs
+++ Assets/Scripts/MimicScripts/MimicSender.cs
@@ -20,7 +20,9 @@ public class MimicSender : MonoBehaviour
     public Vector3 startRotation;
     public Vector3 startPosition;
     public Vector3 movementDirection;
-    bool setup; 
+    bool setup;
+    [SerializeField]
+    bool debugSetup;
    
 
     Vector3 previousPosition;
@@ -144,58 +146,118 @@ public class MimicSender : MonoBehaviour
 
     void setUpBonesArray(Transform gameObjectTransform)
     {
-        bones[0] = gameObjectTransform;       //transform of model itself
-        bones[1] = FindVRIKComponent(gameObjectTransform, "Base HumanPelvis");
-        bones[2] = FindVRIKComponent(gameObjectTransform, "Base HumanLLegThigh");
-        bones[3] = FindVRIKComponent(gameObjectTransform, "Base HumanRLegThigh");
-        bones[4] = FindVRIKComponent(gameObjectTransform, "Base HumanSpine1");
-        bones[5] = FindVRIKComponent(gameObjectTransform, "Base HumanSpine4");
-        bones[6] = FindVRIKComponent(gameObjectTransform, "Base HumanLLegCalf");
-        bones[7] = FindVRIKComponent(gameObjectTransform, "Base HumanRLegCalf");
-        bones[8] = FindVRIKComponent(gameObjectTransform, "Base HumanLLegFoot");
-        bones[9] = FindVRIKComponent(gameObjectTransform, "Base HumanRLegFoot");
-        bones[10] = FindVRIKComponent(gameObjectTransform, "Base HumanFootBone001");
-        bones[11] = FindVRIKComponent(gameObjectTransform, "Base HumanFootBone001 1");
-        bones[12] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmCollarbone");
-        bones[13] = FindVRIKComponent(gameObjectTransform, "Base HumanNeck2");
-        bones[14] = FindVRIKComponent(gameObjectTransform, "Base HumanRCollarbone");
-        bones[15] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmUpperarm");
-        bones[16] = FindVRIKComponent(gameObjectTransform, "Base HumanHead");
-        bones[17] = FindVRIKComponent(gameObjectTransform, "Base HumanRUpperarm");
-        bones[18] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmForearm");
-        bones[19] = FindVRIKComponent(gameObjectTransform, "Base HumanRForearm");
-        bones[20] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmPalm");
-        bones[21] = FindVRIKComponent(gameObjectTransform, "Base HumanRPalm");
-        bones[22] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001");
-        bones[23] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001");
-        bones[24] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001Bone001");
-        bones[25] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002");
-        bones[26] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001");
-        bones[27] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001Bone001");
-        bones[28] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003");
-        bones[29] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001");
-        bones[30] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001Bone001");
-        bones[31] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004");
-        bones[32] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001");
-        bones[33] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001Bone001");
-        bones[34] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005");
-        bones[35] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001");
-        bones[36] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001Bone001");
-        bones[37] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001 1");
-        bones[38] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001 1");
-        bones[39] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001Bone001 1");
-        bones[40] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002 1");
-        bones[41] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001 1");
-        bones[42] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001Bone001 1");
-        bones[43] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003 1");
-        bones[44] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001 1");
-        bones[45] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001Bone001 1");
-        bones[46] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004 1");
-        bones[47] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001 1");
-        bones[48] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001Bone001 1");
-        bones[49] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005 1");
-        bones[50] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001 1");
-        bones[51] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001Bone001 1");
+        if (!debugSetup)
+        {
+            bones[0] = gameObjectTransform;       //transform of model itself
+            bones[1] = FindVRIKComponent(gameObjectTransform, "Base HumanPelvis");
+            bones[2] = FindVRIKComponent(gameObjectTransform, "Base HumanLLegThigh");
+            bones[3] = FindVRIKComponent(gameObjectTransform, "Base HumanRLegThigh");
+            bones[4] = FindVRIKComponent(gameObjectTransform, "Base HumanSpine1");
+            bones[5] = FindVRIKComponent(gameObjectTransform, "Base HumanSpine4");
+            bones[6] = FindVRIKComponent(gameObjectTransform, "Base HumanLLegCalf");
+            bones[7] = FindVRIKComponent(gameObjectTransform, "Base HumanRLegCalf");
+            bones[8] = FindVRIKComponent(gameObjectTransform, "Base HumanLLegFoot");
+            bones[9] = FindVRIKComponent(gameObjectTransform, "Base HumanRLegFoot");
+            bones[10] = FindVRIKComponent(gameObjectTransform, "Base HumanFootBone001");
+            bones[11] = FindVRIKComponent(gameObjectTransform, "Base HumanFootBone001 1");
+            bones[12] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmCollarbone");
+            bones[13] = FindVRIKComponent(gameObjectTransform, "Base HumanNeck2");
+            bones[14] = FindVRIKComponent(gameObjectTransform, "Base HumanRCollarbone");
+            bones[15] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmUpperarm");
+            bones[16] = FindVRIKComponent(gameObjectTransform, "Base HumanHead");
+            bones[17] = FindVRIKComponent(gameObjectTransform, "Base HumanRUpperarm");
+            bones[18] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmForearm");
+            bones[19] = FindVRIKComponent(gameObjectTransform, "Base HumanRForearm");
+            bones[20] = FindVRIKComponent(gameObjectTransform, "Base HumanLArmPalm");
+            bones[21] = FindVRIKComponent(gameObjectTransform, "Base HumanRPalm");
+            bones[22] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001");
+            bones[23] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001");
+            bones[24] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001Bone001");
+            bones[25] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002");
+            bones[26] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001");
+            bones[27] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001Bone001");
+            bones[28] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003");
+            bones[29] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001");
+            bones[30] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001Bone001");
+            bones[31] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004");
+            bones[32] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001");
+            bones[33] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001Bone001");
+            bones[34] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005");
+            bones[35] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001");
+            bones[36] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001Bone001");
+            bones[37] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001 1");
+            bones[38] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001 1");
+            bones[39] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone001Bone001Bone001 1");
+            bones[40] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002 1");
+            bones[41] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001 1");
+            bones[42] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone002Bone001Bone001 1");
+            bones[43] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003 1");
+            bones[44] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001 1");
+            bones[45] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone003Bone001Bone001 1");
+            bones[46] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004 1");
+            bones[47] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001 1");
+            bones[48] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone004Bone001Bone001 1");
+            bones[49] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005 1");
+            bones[50] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001 1");
+            bones[51] = FindVRIKComponent(gameObjectTransform, "Base HumanPalmBone005Bone001Bone001 1");
+        }
+        else
+        {
+            bones[0] = FindVRIKComponent(gameObjectTransform, "ChildRig");
+            //  bones[0] = gameObjectTransform;       //transform of model itself
+            bones[1] = FindVRIKComponent(gameObjectTransform, "J_Lumbar");
+            bones[2] = FindVRIKComponent(gameObjectTransform, "L_Femur");
+            bones[3] = FindVRIKComponent(gameObjectTransform, "R_Femur");
+            bones[4] = FindVRIKComponent(gameObjectTransform, "J_Dorsal1");
+            bones[5] = FindVRIKComponent(gameObjectTransform, "L_Knee");
+            bones[6] = FindVRIKComponent(gameObjectTransform, "R_Knee");
+            bones[7] = FindVRIKComponent(gameObjectTransform, "J_Dorsal2");
+            bones[8] = FindVRIKComponent(gameObjectTransform, "L_rotFoot1");
+            bones[9] = FindVRIKComponent(gameObjectTransform, "R_rotFoot1");
+            bones[10] = FindVRIKComponent(gameObjectTransform, "L_jt14");
+            bones[11] = FindVRIKComponent(gameObjectTransform, "R_jt14");
+            bones[12] = FindVRIKComponent(gameObjectTransform, "L_Clavicle");
+            bones[13] = FindVRIKComponent(gameObjectTransform, "J_Neck1");
+            bones[14] = FindVRIKComponent(gameObjectTransform, "R_Clavicle");
+            bones[15] = FindVRIKComponent(gameObjectTransform, "L_Arm");
+            bones[16] = FindVRIKComponent(gameObjectTransform, "J_Head");
+            bones[17] = FindVRIKComponent(gameObjectTransform, "R_Arm");
+            bones[18] = FindVRIKComponent(gameObjectTransform, "L_Forearm");
+            bones[19] = FindVRIKComponent(gameObjectTransform, "R_Forearm");
+            bones[20] = FindVRIKComponent(gameObjectTransform, "L_Hand");
+            bones[21] = FindVRIKComponent(gameObjectTransform, "R_Hand");
+            bones[22] = FindVRIKComponent(gameObjectTransform, "L_Index1");
+            bones[23] = FindVRIKComponent(gameObjectTransform, "L_Index2");
+            bones[24] = FindVRIKComponent(gameObjectTransform, "L_Index3");
+            bones[25] = FindVRIKComponent(gameObjectTransform, "L_Little1");
+            bones[26] = FindVRIKComponent(gameObjectTransform, "L_Little2");
+            bones[27] = FindVRIKComponent(gameObjectTransform, "L_Little3");
+            bones[28] = FindVRIKComponent(gameObjectTransform, "L_Middle1");
+            bones[29] = FindVRIKComponent(gameObjectTransform, "L_Middle2");
+            bones[30] = FindVRIKComponent(gameObjectTransform, "L_Middle3");
+            bones[31] = FindVRIKComponent(gameObjectTransform, "L_Thumb3");
+            bones[32] = FindVRIKComponent(gameObjectTransform, "L_Thumb1");
+            bones[33] = FindVRIKComponent(gameObjectTransform, "L_Thumb2");
+            bones[34] = FindVRIKComponent(gameObjectTransform, "L_Third1");
+            bones[35] = FindVRIKComponent(gameObjectTransform, "L_Third2");
+            bones[36] = FindVRIKComponent(gameObjectTransform, "L_Third3");
+            bones[37] = FindVRIKComponent(gameObjectTransform, "R_Index1");
+            bones[38] = FindVRIKComponent(gameObjectTransform, "R_Index2");
+            bones[39] = FindVRIKComponent(gameObjectTransform, "R_Index3");
+            bones[40] = FindVRIKComponent(gameObjectTransform, "R_Little1");
+            bones[41] = FindVRIKComponent(gameObjectTransform, "R_Little2");
+            bones[42] = FindVRIKComponent(gameObjectTransform, "R_Little3");
+            bones[43] = FindVRIKComponent(gameObjectTransform, "R_Middle1");
+            bones[44] = FindVRIKComponent(gameObjectTransform, "R_Middle2");
+            bones[45] = FindVRIKComponent(gameObjectTransform, "R_Middle3");
+            bones[46] = FindVRIKComponent(gameObjectTransform, "R_Thumb3");
+            bones[47] = FindVRIKComponent(gameObjectTransform, "R_Thumb1");
+            bones[48] = FindVRIKComponent(gameObjectTransform, "R_Thumb2");
+            bones[49] = FindVRIKComponent(gameObjectTransform, "R_Third1");
+            bones[50] = FindVRIKComponent(gameObjectTransform, "R_Third2");
+            bones[51] = FindVRIKComponent(gameObjectTransform, "R_Third3");
+
+        }
     }
 
     private void SendBones()
